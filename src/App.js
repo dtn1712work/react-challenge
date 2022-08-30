cce9f17563dbda3af1a4c729f8c4018a9122328a
dtn1712work <dtn1712.work@gmail.com>
2022-08-30 16:01:27 -0700

    test-cors error fixed with chrome extension for now.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import * as React from "react";
-
-import "./App.css";
+import './App.css';
 
 // Questions:
 // 1. Load data from local file (path: “https://ac.aws.citizennet.com/assets/qspreviews/qs_interview_data.json”)
@@ -8,52 +6,14 @@ import "./App.css";
 // 3. Add a hover state with a dark, semi-transparent overlay and display the ID of the hovered brand.
 
 function App() {
-  const [data, setData] = React.useState([]);
-
-  React.useEffect(() => {
-    const url =
-      "https://ac.aws.citizennet.com/assets/qspreviews/qs_interview_data.json";
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url, {
-          method: "GET",
-          mode: "cors"
-        });
-
-        const json = await response.json();
-        
-        const sortedData = json.data.sort(
-          (a, b) => b.source_items.audience_size - a.source_items.audience_size
-        );
-
-        setData(sortedData);
-
-      } catch (e) {
-        console.log("err: ", e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="app">
-      <h4>Choose a Condé Nast brand's audience:</h4>
-
-      <div className="grid">
-        {data.map((item) => (
-          <div className="item" key={item.source_items.id}>
-            <img
-              src={item.social_media_pages.picture}
-              alt={item.social_media_pages.name}
-            />
-            <div className="overlay">
-              <div className="text">{item.source_items.id}</div>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Let's start here
+        </div>
+        
+      </header>
     </div>
   );
 }
